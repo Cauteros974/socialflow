6726f7cf8233dba198a605127e22c8934253cecf
Cauteros974 <pusik6666@ukr.net>
2025-10-19 19:03:01 +0300

    Update useAuthStore.ts

--- src/store/useAuthStore.ts
+++ src/store/useAuthStore.ts
@@ -1,4 +1,4 @@
-import create from 'zustand';
+import { create } from 'zustand';
 import { type AppUser } from '../types/user';
 
 interface AuthState {
@@ -7,3 +7,12 @@ interface AuthState {
     setUser: (user: AppUser | null) => void;
     setLoading: (isLoading: boolean) => void;
 }
+
+export const useAuthStore = create<AuthState>((set) =>({
+    user: null,
+
+    isLoading: true,
+
+    setUser: (user) => set({ user, isLoading: false }),
+    setLoading: (isLoading) => set({ isLoading}),
+}));
