ad3cb41c5aaf506ad06ae2cbb7f83df01489eabb
Cauteros974 <pusik6666@ukr.net>
2025-10-20 22:02:55 +0300

    Update authSchemas.ts

--- src/types/authSchemas.ts
+++ src/types/authSchemas.ts
@@ -5,7 +5,6 @@ export default registerSchema = z.object({
         .string()
         .min(3, 'The name must be at least 3 characters long.')
         .max(20, 'The name must be at least 20 characters short.')
-    email: z.string().email('Incorrect email'),
-    password: z.string().min(6, 'The password cannot be empty'),
-
+    email: z.string().email('Invalid email'),
+    password: z.string().min(6, 'Password must be at least 6 characters'),
 });
