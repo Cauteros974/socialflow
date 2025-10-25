c568580185f0f5220cf6f6a62d7f6a5d51db404c
Cauteros974 <pusik6666@ukr.net>
2025-10-25 20:18:49 +0300

    Update useThemeStore.ts

--- src/store/useThemeStore.ts
+++ src/store/useThemeStore.ts
@@ -4,3 +4,8 @@ interface ThemeState {
     theme: 'light' | 'dark';
     setTheme: (theme: 'light' | 'dark') => void;
 }
+
+export const useThemeStore = create <ThemeState>((set) => ({
+    theme: 'light',
+    setTheme: (theme) => set({ theme }),
+}));
