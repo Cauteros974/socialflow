aff8fc4a0138f99a8d720350a7d14193b9271c81
Cauteros974 <pusik6666@ukr.net>
2025-10-25 19:29:12 +0300

    Update post.ts

--- src/types/post.ts
+++ src/types/post.ts
@@ -1,16 +1,20 @@
 import { type AppUser} from './user';
 import { type Comment } from './comment';
 
+
+export type PostAuthor = Pick<AppUser, 'uid' | 'displayName' | 'photoUrl'>;
+
+
 /**
 * Post structure.
 * In mock mode, we store comments directly in the post.
 */
 export interface Post {
-    id: string;
-    text: string;
-    imageUrl: string | null;
-    author: AppUser;
-    likes: string[];
-    comments: Comment[];
-    createdAt: number;
+  id: string;
+  text: string;
+  imageUrl: string | null;
+  author: PostAuthor;
+  likes: string[];
+  comments: Comment[];
+  createdAt: number;
 }
