6d426a830a9ede41a88d68b138a65a12003f8b9a
Cauteros974 <pusik6666@ukr.net>
2025-10-26 17:15:33 +0200

    commit

--- src/hooks/usePost.ts
+++ src/hooks/usePost.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
-import { postService } from "../services/postService";
+import { useQuery } from '@tanstack/react-query';
+import { postService } from '../services/postService';
 
-export const usePosts = () => useQuery({ queryKey: ['posts'], queryFn: postService.fetchPosts });
+export const usePost = (postId: string) => useQuery({ queryKey: ['post', postId], queryFn: () => postService.fetchPostById(postId), enabled: !!postId });
