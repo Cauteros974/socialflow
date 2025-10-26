165fa1c13ab1e2895ad50a64693adfa8ee2e541e
Cauteros974 <pusik6666@ukr.net>
2025-10-26 17:08:28 +0200

    Update usePostStore.ts

--- src/store/usePostStore.ts
+++ src/store/usePostStore.ts
@@ -1,7 +1,5 @@
-import { create } from "zustand";
-
-interface PostUiState{
-
+import { create } from 'zustand';
+interface PostUiState {
+  // e.g. filter: 'all' | 'following';
 }
-
 export const usePostStore = create<PostUiState>(() => ({}));
