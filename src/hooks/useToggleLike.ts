5895d124f99ea29b80bbc5401b91e2e53312c809
Cauteros974 <pusik6666@ukr.net>
2025-10-26 17:44:19 +0200

    Update useToggleLike.ts

--- src/hooks/useToggleLike.ts
+++ src/hooks/useToggleLike.ts
@@ -7,7 +7,14 @@ import { toast } from 'react-hot-toast';
 export const useToggleLike = (postId: string) => {
     const qc = useQueryClient();
     const user = useAuthStore.getState().user;
-    const currentUser = user?.uid;
+    const currentUserId = user?.uid;
 
-    
+    return useMutation ({
+        mutationFn: () => {
+            if (!currentUserId) { toast.error('You need to log in to like'); throw new Error('Not auth'); }
+            return postService.toggleLikePost(postId, currentUserId);
+        },
+
+        
+    })
 }
