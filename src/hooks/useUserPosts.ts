867b3822e541d50ffd12e1d0ead929319b9ff15f
Cauteros974 <pusik6666@ukr.net>
2025-10-26 17:23:01 +0200

    Update useUserPosts.ts

--- src/hooks/useUserPosts.ts
+++ src/hooks/useUserPosts.ts
@@ -1,2 +1,9 @@
 import { useQuery } from '@tanstack/react-query';
 import { postService } from '../services/postService';
+
+export const usePost = (userId: string) =>
+  useQuery({
+    queryKey: ['user', userId],
+    queryFn: () => postService.fetchPostById(userId),
+    enabled: !!userId,
+});
