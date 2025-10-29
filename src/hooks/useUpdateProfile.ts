db93d15e44a009979a346eccd8281ab59f319462
Cauteros974 <pusik6666@ukr.net>
2025-10-29 18:54:49 +0200

    Update useUpdateProfile.ts

--- src/hooks/useUpdateProfile.ts
+++ src/hooks/useUpdateProfile.ts
@@ -5,17 +5,17 @@ import { EditProfileSchema } from '../types/schemas';
 import { toast } from 'react-hot-toast';
 import { getErrorMessage } from '../utils/handleError';
 
-export const useUpdateProfile = (useId: string) => {
-    const qc = useQueryClient();
-    const updateUserInStore = useAuthStore((s) => s.updateUser);
-    return useMutation({
-        mutationFn: (data:EditProfileSchema) => useAuthStore.updateUserProfile(useId, data),
-        onSuccess: (updateUser) => {
-            updateUserInStore(updatedUser);
-            qc.setQueryData(['userProfile', userId], updatedUser);
-            qc.invalidateQueries({ queryKey: ['posts'] });
-            toast.success('Profile updated successfully!');
-        },
-        onError: (err) => toast.error(getErrorMessage(err)),
-    });
+export const useUpdateProfile = (userId: string) => {
+  const qc = useQueryClient();
+  const updateUserInStore = useAuthStore((s) => s.updateUser);
+  return useMutation({
+    mutationFn: (data: EditProfileSchema) => authService.updateUserProfile(userId, data),
+    onSuccess: (updatedUser) => {
+      updateUserInStore(updatedUser);
+      qc.setQueryData(['userProfile', userId], updatedUser);
+      qc.invalidateQueries({ queryKey: ['posts'] });
+      toast.success('Профиль успешно обновлен!');
+    },
+    onError: (err) => toast.error(getErrorMessage(err)),
+  });
 };
