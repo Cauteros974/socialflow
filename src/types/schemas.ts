72f76c09dc14ad81f2c5b0a97c30f65203a18e79
Cauteros974 <pusik6666@ukr.net>
2025-10-29 18:59:55 +0200

    Update schemas.ts

--- src/types/schemas.ts
+++ src/types/schemas.ts
@@ -16,5 +16,12 @@ export const loginSchema = z.object({
   password: z.string().min(1, { message: 'The password cannot be empty.' }),
 });
 
+export const editProfileSchema = z.object({
+  displayName: z
+    .string()
+    .min(3, { message: 'The name must be at least 3 characters long.' })
+    .max(20, { message: 'The name must be at most 20 characters long.' }),
+})
+
 export type RegisterFormValues = z.infer<typeof registerSchema>;
 export type LoginFormValues = z.infer<typeof loginSchema>;
