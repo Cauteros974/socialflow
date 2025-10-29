7e777bdf5059f811a86534af5fea5f080504a33c
Cauteros974 <pusik6666@ukr.net>
2025-10-29 21:13:07 +0200

    Update MainLayout.tsx

---
+++ src/components/layout/MainLayout.tsx
@@ -0,0 +1,17 @@
+import { Outlet } from 'react-router-dom';
+import { Header } from './Header';
+import { Sidebar } from './Sidebar';
+
+export const MainLayout = () => {
+    return(
+        <div className="app-layout flex">
+            <Sidebar />
+            <div className="flex-1">
+                <Header />
+                <div className="p-4">
+                    <Outlet />
+                </div>
+            </div>
+        </div>
+    );
+};
