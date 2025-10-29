9553b8c791343a615e7764e909bb52178629a51d
Cauteros974 <pusik6666@ukr.net>
2025-10-29 21:20:36 +0200

    Update Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -5,7 +5,7 @@ export const Header = () => {
     const { user } = useAuthStore();
 
     return(
-        <div className="flex justify-between items-center p-4 shadow-sm border-b">
+        <header className="flex justify-between items-center p-4 shadow-sm border-b">
             <Link to="/" className="text-xl fond-bold">SocialFlow</Link>
             <div className="flex items-center gap-3">
                 {user && (
@@ -16,10 +16,14 @@ export const Header = () => {
                         alt={user.displayName}
                         className="w-8 h-8 rounded-full object-cover"
                         />
+                        <span>{user.displayName}</span>
+                        </Link>
+                        <Link to="/create-post" className="px-3 py-1 bg-blue-600 text-white rounded-md">
+                            + Post
                         </Link>
                     </>
                 )}
             </div>
-        </div>
+        </header>
     );
 };
