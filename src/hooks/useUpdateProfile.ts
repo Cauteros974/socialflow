eefa1b456b25f71a01077abbe777bee9d2a0110c
Cauteros974 <pusik6666@ukr.net>
2025-10-29 18:46:13 +0200

    commit

---
+++ src/hooks/useUpdateProfile.ts
@@ -0,0 +1,5 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { postService } from "../services/postService";
+import { type Post } from "../types/post";
+import { type AppUser } from "../types/user";
+
--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,13 +1,8 @@
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
-
+import 'dayjs/locale/en';
 dayjs.extend(relativeTime);
 dayjs.locale('en');
 
-export const formatDateFromNoew = (timestamp: number): string => {
-    return dayjs(timestamp).fromNow();
-};
-
-export const formatFullDate = (timestamp: number): string => {
-    return dayjs(timestamp).format('D MMMM YYYY., HH:mm');
-};
+export const formatDateFromNow = (timestamp: number) => dayjs(timestamp).fromNow();
+export const formatFullDate = (timestamp: number) => dayjs(timestamp).format('D MMMM YYYY, HH:mm');
