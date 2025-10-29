944af190d28b9926d264d4df12472f69823e7c38
Cauteros974 <pusik6666@ukr.net>
2025-10-29 21:43:03 +0200

    Update ProfilePage.tsx

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -5,4 +5,10 @@ import { useUserPosts } from '../hooks/useUserPosts';
 import { ProfileHeader } from '../components/profile/ProfileHeader';
 import { PostGrid } from '../components/profile/PostGrid';
 import { Loader2 } from 'lucide-react';
+import { string } from "zod";
 
+export const ProfilePage = () => {
+    const { uid } = useParams <{ uid: string}>();
+    const { data: user, isLoading: userLoading } = useUserPosts(uid!);
+    const { data: posts, isLoading: postsLoading } = useUserPosts(uid!);
+}
