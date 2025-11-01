44a865885869b28591000fbf8036e2726ab1aa3e
Cauteros974 <pusik6666@ukr.net>
2025-11-01 09:59:23 +0200

    Create Label.tsx

---
+++ src/components/ui/Label.tsx
@@ -0,0 +1,2 @@
+import React from 'react';
+export const Label: React.FC<React.LabelHTMLAttributes<HTMLLabelElement>> = ({ children, className='', ...props }) => <label className={`block text-sm font-medium ${className}`} {...props}>{children}</label>;
