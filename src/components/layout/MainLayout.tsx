2c7061746bec1ad54f114a21d4fa4e3b23289992
Cauteros974 <pusik6666@ukr.net>
2025-11-02 17:03:20 +0200

    Update MainLayout.tsx

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,17 +1,10 @@
+import React from 'react';
 import { Outlet } from 'react-router-dom';
 import { Header } from './Header';
-import { Sidebar } from 'lucide-react';
 
 export const MainLayout = () => {
-    return(
-        <div className="app-layout flex">
-            <Sidebar />
-            <div className="flex-1">
-                <Header />
-                <div className="p-4">
-                    <Outlet />
-                </div>
-            </div>
-        </div>
-    );
+    <>
+        <Header />
+        <main style = {{ padding: '1rem'}}> <Outlet /> </main>
+    </>
 };
