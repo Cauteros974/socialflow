a4b83388f96b22776eb22b392742460caa8bdae7
Cauteros974 <pusik6666@ukr.net>
2025-11-02 17:51:10 +0200

    Update MainLayout.tsx

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -13,13 +13,18 @@ export const MainLayout = () => {
             <nav className="nav-container">
                 <Link to="/" className="logo">SocialFlow</Link>
                 <div className="nav-links">
-                    (user ?(
+                    {user ?(
                         <>
                             <Link to={`/profile/${user.uid}`}>Profile</Link>
                             <Link to="/create-post">New Post</Link>
                             <Button variant="danger" onClick={logout}>LogOut</Button>
                         </>
-                    ))
+                    ):(
+                        <>
+                            <Link to="/login">LogIn</Link>
+                            <Link to="/register">Registration</Link>
+                        </>
+                    )}
                 </div>
             </nav>
             </header>
