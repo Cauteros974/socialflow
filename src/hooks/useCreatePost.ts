2b44c6ef7ab5d9251132721d62744992b6aaebab
Cauteros974 <pusik6666@ukr.net>
2025-11-08 17:02:41 +0200

    Update useCreatePost.ts

--- src/hooks/useCreatePost.ts
+++ src/hooks/useCreatePost.ts
@@ -11,6 +11,22 @@ export const useCreatePost = () => {
   return useMutation({
     mutationFn: async (data: CreatePostSchema) => {
       if (!user) throw new Error('You must be logged in to create a post.');
-    }
-  })
+
+      //Pass the author inside
+      return await postService.createPost({
+        ...data,
+        author: user,
+        imageUrl: data.imageUrl ?? null,
+      });
+    },
+
+    onSuccess: () => {
+      toast.success('The post has been published');
+      queryCliernt.invalidateQueries({queryKey: ['posts']}); //updating the list of posts
+    },
+
+    onError: (err) => {
+      toast.error(err instanceof Error ? err.message: 'Failed to create post');
+    },
+  });
 };
