802a2bd071dd70db884b19dfce423d1c447386cd
Cauteros974 <pusik6666@ukr.net>
2025-11-08 16:48:54 +0200

    Update usePostStore.ts

--- src/store/usePostStore.ts
+++ src/store/usePostStore.ts
@@ -1,5 +1,12 @@
 import { create } from 'zustand';
+import { type Post } from '../types/post';
+import { type Author } from '../types/user';
+
 interface PostUiState {
-  // e.g. filter: 'all' | 'following';
+  posts: Post[];
+  addPost: (post: Post) => void;
+  setPosts: (posts: Post[]) => void;
+  toggleLike: (postId: string, userId: string) => void;
+  addComment: (postId: string, text: string, author: Author) => void;
 }
-export const usePostStore = create<PostUiState>(() => ({}));
+
