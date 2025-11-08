514b570c6096bb1d69ff481bc971411958c14498
Cauteros974 <pusik6666@ukr.net>
2025-11-08 16:29:33 +0200

    Update PostGrid.tsx

--- src/components/profile/PostGrid.tsx
+++ src/components/profile/PostGrid.tsx
@@ -9,7 +9,7 @@ export const PostGrid: React.FC<{posts: Post[ ]}> = ({ posts }) => {
         <div style={{display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', gap:8}}>
             {posts.map(p => (
                 <Link to={`/post/${p.id}`} key={p.id} style={{ position:'relative', paddingTop:'100%', borderRadius:8, overflow:'hidden', background:'#f3f3f3' }}>
-                    
+                    {p.imageUrl ? <img src={p.imageUrl} alt={p.text.slice(0,20)} style={{ position:'absolute', inset:0, width:'100%', height:'100%', objectFit:'cover' }} /> : <div style={{ position:'absolute', inset:0, display:'flex',alignItems:'center',justifyContent:'center' }}><FileText /></div>}
                 </Link>
             ))}
         </div>
