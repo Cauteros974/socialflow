664499e5d62f9be0999484aecf389c0add31487d
Cauteros974 <pusik6666@ukr.net>
2025-11-09 15:27:14 +0200

    Update PostSkeleton.tsx

--- src/components/ui/PostSkeleton.tsx
+++ src/components/ui/PostSkeleton.tsx
@@ -13,7 +13,9 @@ export const PostSkeleton = () => {
                 <div className="w-10 h-10 bg-gray-200 rounded-full" />
                 <div className="w-32 h-4 bg-gray-200 rounded" /> 
             </div>
-
+            <div className="w-full h-4 bg-gray-200 rounded mb-2" />
+            <div className="w-3/4 h-4 bg-gray-200 rounded" />
+            <div className="w-full h-48 bg-gray-200 rounded-xl mt-4" />
         </motion.div>
-    )
-}
+    );
+};
