b1ad608fe165fe209656d97d351cbc6b9aba2d63
Cauteros974 <pusik6666@ukr.net>
2025-11-09 17:46:21 +0200

    commit

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -28,7 +28,6 @@ export const MainLayout = () => {
                 </div>
             </nav>
             </header>
-
             <main className="main-content">
                 <Outlet />
             </main>
