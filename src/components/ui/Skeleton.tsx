7faf98fbc72f1141bba9557e4c5a0259931307b6
Cauteros974 <pusik6666@ukr.net>
2025-11-09 15:04:57 +0200

    Update Skeleton.tsx

--- src/components/ui/Skeleton.tsx
+++ src/components/ui/Skeleton.tsx
@@ -5,6 +5,7 @@ import "react-loading-skeleton/dist/skeleton.css";
 export const PostSkeleton = () => {
     <div className="rounded-2xl p-4 bg-white/5 dark:bg-black/10 mb-4">
         <Skeleton circle width={48} height={48} />
-        <Skeleton height={20} width="60%" />
+        <Skeleton height={20} width="60%" style={{ margin: "8px 0"}}/>
+        <Skeleton height={250}/>
     </div>
-}
+};
