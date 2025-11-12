06326e69a75c1980eff72bf2cd196aae8e6ad737
Cauteros974 <pusik6666@ukr.net>
2025-11-12 20:09:35 +0200

    commit

--- src/components/ui/FadeIn.tsx
+++ src/components/ui/FadeIn.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import './FadeIn.css';
 
-export const FadeIn: React.FC<{ children: React.ReactNode}> = ({ children }) => {
+export const FadeIn: React.FC<{ children: React.ReactNode}> = ({ children }) => (
     <div className="fade-in">{children}</div>
-};
+);
