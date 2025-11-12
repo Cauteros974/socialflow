6ae385b723e0a8b6b070de6f66eae1ae55aa7426
Cauteros974 <pusik6666@ukr.net>
2025-11-12 19:29:15 +0200

    Update ProfilePage.tsx

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -5,11 +5,15 @@ import { useUserPosts } from '../hooks/useUserPosts';
 import { ProfileHeader } from '../components/profile/ProfileHeader';
 import { PostGrid } from '../components/profile/PostGrid';
 import { Loader2 } from 'lucide-react';
+import { type Post } from '../types/post';
 
 export const ProfilePage = () => {
   const { uid } = useParams<{ uid: string }>();
   const { data: user, isLoading: userLoading } = useUserProfile(uid!);
-  const { data: posts, isLoading: postsLoading } = useUserPosts(uid!);
+  const { data: postsData, isLoading: postsLoading } = useUserPosts(uid!);
+
+  // Converting data into an array of posts
+  const posts: Post[] = Array.isArray(postsData) ? postsData : postsData ? [postsData] : [];
 
   if (userLoading || postsLoading) {
     return (
@@ -25,8 +29,8 @@ export const ProfilePage = () => {
 
   return (
     <div style={{ maxWidth: 800, margin: '0 auto', padding: '1rem' }}>
-      <ProfileHeader profile={user} postCount={posts?.length || 0} />
-      <PostGrid posts={posts || []} />
+      <ProfileHeader profile={user} postCount={posts.length} />
+      <PostGrid posts={posts} />
     </div>
   );
 };
