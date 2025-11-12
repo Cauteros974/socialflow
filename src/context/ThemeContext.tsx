d5bf5144f06fc971dfaba6cdb48e8ff765cedcce
Cauteros974 <pusik6666@ukr.net>
2025-11-12 20:48:17 +0200

    Update ThemeContext.tsx

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -1,7 +1,16 @@
 import React, {createContext, useContext, useEffect, useState} from "react";
 
 type Theme = 'light' | 'dark';
-interface ThemeContext {
+interface ThemeContextType {
     theme: Theme;
     toggleTheme: () => void;
+}
+
+const ThemeContext = createContext<ThemeContextType | null>(null);
+
+export const ThemeProvider = ({ children }: {children: React.ReactNode}) => {
+    const [theme, setTheme] = useState<Theme>(
+        (localStorage.getItem('theme') as Theme) || 'light'
+    );
+    
 }
