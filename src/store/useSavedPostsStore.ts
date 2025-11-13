872b7fbb7d7f4e39266bf70b2e8d2c31bf30727c
Cauteros974 <pusik6666@ukr.net>
2025-11-13 20:49:42 +0200

    Update useSavedPostsStore.ts

--- src/store/useSavedPostsStore.ts
+++ src/store/useSavedPostsStore.ts
@@ -1,9 +1,21 @@
 import { create } from "zustand";
-import { persit } from 'zustand/middleware';
+import { persist } from 'zustand/middleware';
 import { type Post } from "../types/post";
 
 interface SavedPostState {
-    savedPost: Post[];
+    savedPosts: Post[];
     toggleSave: (post: Post) => void;
     isSaved: (id: string) => boolean;
 }
+
+export const useSavedPostsStore = create <SavedPostState>()(
+    persist(
+        (set, get) => ({
+            savedPost: [],
+            toggleSave: (post) => {
+                const { savedPosts } = get();
+                const exists = savedPosts.find((p) => p.id === post.id);
+            }
+        })
+    )
+)
