3e9bd05bd63bec689bc3ffafdf2c323b72461f7a
Cauteros974 <pusik6666@ukr.net>
2025-11-13 20:22:01 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import App from './App';
 import { ThemeProvider } from './context/ThemeContext';
+import App  from './App';
 import './index.css';
 
-const queryClient = new QueryClient();
+const client = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
+    <QueryClientProvider client={client}>
       <ThemeProvider>
-      <App />
+        <App />
       </ThemeProvider>
     </QueryClientProvider>
   </React.StrictMode>
