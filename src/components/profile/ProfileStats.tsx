3215470ce5ea20954744d2b65311d31d57d8cbe4
Cauteros974 <pusik6666@ukr.net>
2025-11-13 20:32:49 +0200

    Update ProfileStats.tsx

--- src/components/profile/ProfileStats.tsx
+++ src/components/profile/ProfileStats.tsx
@@ -1,11 +1,8 @@
 import React from "react";
 
-export const ProfileStats = ({ posts, followers, following }) => {
-    return(
-        <div className="profile-stats">
-            <div><strong>{posts}</strong><span>Posts</span></div>
-            <div><strong>{followers}</strong><span>Followers</span></div>
-            <div><strong>{following}</strong><span>Following</span></div>
-        </div>
-    );
-};
+interface ProfileStateProps {
+    posts: number;
+    followers: number;
+    following: number;
+}
+
