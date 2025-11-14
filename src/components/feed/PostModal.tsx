c653488e1e4ba10ec05e608f56705f686eaff1b8
Cauteros974 <pusik6666@ukr.net>
2025-11-14 19:43:09 +0200

    Update PostModal.tsx

--- src/components/feed/PostModal.tsx
+++ src/components/feed/PostModal.tsx
@@ -6,13 +6,15 @@ interface PostModalProps{
     onClose: () => void;
 }
 
-export const PostModal = ({ post, onClose }) => {
+export const PostModal: React.FC<PostModalProps> = ({ post, onClose }) => {
     if(!post) return null;
 
     return createPortal(
         <div className="modal-overlay" onClick={onClose}>
             <div className="modal-content" onClick={e => e.stopPropagation()}>
-                <img src={post.imageUrl} alt="" className="modal-img" />
+                {post.imageUrl && (
+                    <img src={post.imageUrl} alt="" className="modal-img" />
+                )}
                 <p className="modal-text">{post.text}</p>
             </div>
         </div>,
