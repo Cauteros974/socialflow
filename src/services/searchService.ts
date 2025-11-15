f3e0bf8dff5fe1a6a039ce328398ff21d7b25e92
Cauteros974 <pusik6666@ukr.net>
2025-11-15 19:36:46 +0200

    Create searchService.ts

---
+++ src/services/searchService.ts
@@ -0,0 +1,14 @@
+import { mockPosts } from "./mockData";
+import { getUsersFromStorage } from "./authService";
+import { simulateDelay } from "./mockData";
+
+export const searchService = {
+    searchUsers: async (query: string) => {
+        await simulateDelay(200);
+
+        const users = getUsersFromStorage();
+        const q = query.toLowerCase();
+
+        
+    }
+}
